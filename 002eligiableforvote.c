a367c6cdbde85e7772c653be5b35ed255a3ad45d
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-15 14:58:28 +0530

    Add files via upload

---
+++ 002eligiableforvote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("enter a number");
+ scanf("%d",&a);
+
+ if(a>=18)
+ printf("eligiable for vote");
+
+ else
+ printf("not eligiable for vote");
+
+ return 0;
+}
+
