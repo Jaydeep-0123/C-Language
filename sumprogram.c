4caa70f97248d5ce4d5657533e54bbe719a1dea0
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-15 11:55:39 +0530

    Add files via upload

---
+++ sumprogram.c
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+int main()
+{
+ 
+int a,b,c;
+printf("enter number");
+scanf("%d",&a);
+printf("enter a number");
+scanf("%d",&b);
+c=a+b;
+printf("\nthe sum of = %d",c);
+return 0;
+}*/
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    
+    printf("Hello, world!");
+    scanf("\n%s",s);
+    
+    printf("%s",s);
+    return 0;
+}
