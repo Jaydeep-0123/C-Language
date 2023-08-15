a065867f0531041553f9c73a4f8919ce98e7c603
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-15 22:12:03 +0530

    Add files via upload

---
+++ 009_SumOfDigit.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+
+  cout<<"enter the first number";
+  cin>>a;
+
+  cout<<"enter the second number";
+  cin>>b;
+  
+ cout<<"enter the third number";
+ cin>>c;
+
+ 
+int d=a+b+c;
+int e=a-b-c;
+int f=a*b*c;
+
+cout<<"\nthe addition of = "<<d;
+cout<<"\nthe substriction of ="<<e;
+cout<<"\nthe multiplication of ="<<f;
+
+return 0;
+}
