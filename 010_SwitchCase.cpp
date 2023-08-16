0c6484e21aaa013f8d82713f79fe0585e8e65215
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-16 21:09:28 +0530

    Add files via upload

---
+++ 010_SwitchCase.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter a number";
+  cin>>a;
+
+ switch(a)
+ {
+ case 1:
+ cout<<"Monday";
+ break;
+
+ case 2:
+ cout<<"Tuesday";
+ break;
+
+ case 3:
+ cout<<"Wednesday";
+ break;
+
+ case 4:
+ cout<<"Thursday";
+ break;
+
+ case 5:
+ cout<<"Friday";
+ break;
+
+ case 6:
+ cout<<"Suterday";
+ break;
+
+ default:
+ cout<<"invalid number";
+}
+return 0;
+}
+
+ 
+ 
