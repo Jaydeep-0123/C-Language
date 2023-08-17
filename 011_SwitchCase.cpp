852dd5672c74a8f6be59cace7bdb1100351940a9
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-17 14:47:10 +0530

    Add files via upload

---
+++ 011_SwitchCase.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char ch;
+ cout<<"enter the charecter";
+ cin>>ch;
+
+ switch(ch)
+ {
+  case 'j':
+  cout<<"you are jaydeep";
+  break;
+
+  case 'k':
+  cout<<"you are kuldeep";
+  break;
+
+  case 'r':
+  cout<<"you are rohan";
+  break;
+
+  case 'n':
+  cout<<"you are krishna";
+  break;
+
+ default:
+ cout<<"invalid number";
+}
+return 0;
+}
