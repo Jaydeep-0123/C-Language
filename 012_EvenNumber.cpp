02ca0cb6f1c300c67c43e7f07ee48d9a03fd3830
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-18 15:26:22 +0530

    Add files via upload

---
+++ 012_EvenNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,i;
+
+ cout<<"enter a number";
+ cin>>a;
+
+ for(i=1;i<=a;i++)
+ {
+   if(i%2==0)
+   
+  cout<<" "<<i;
+ }
+return 0;
+}
+
+       
+
