be0682cf4d81be7778966ace4dba49788c85165f
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-19 21:35:46 +0530

    Create 011_SumOfNNumber.cpp

---
+++ 011_SumOfNNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+ int n,sum=0;
+
+ cout<<"enter a number";
+ cin>>n;
+
+ for(int i=1;i<=n;i++)
+ {
+   sum=sum+i;
+ }
+     cout<<sum;
+return 0;
+}
