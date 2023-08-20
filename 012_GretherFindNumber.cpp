1a24374e4f1292eeee523c70935938a7da03625c
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-20 17:25:19 +0530

    Create 012_GretherFindNumber.cpp

---
+++ 012_GretherFindNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+  int a,b;
+  cout<<"enter a first number";
+  cin>>a;
+  cout<<"enter a second number";
+  cin>>b;
+
+  if(a>b)
+  cout<<"a is Grether from b";
+  else
+  cout<<"b is Grether from a";
+
+ return 0;
+}
