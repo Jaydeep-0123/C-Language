0e53cbcb30804cbaa617235c2df64958425ab6ca
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-21 11:38:02 +0530

    Add files via upload

---
+++ 015_PrintArraysvalue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+  int a[10]={10,20,30,40,50,60,70,80,90,100};
+  int i;
+
+ for(i=0;i<10;i++)
+ {
+   cout<<"the arrays value are"<<" "<<a[i];
+   cout<<"\n";
+ }
+
+ return 0;
+}
+
