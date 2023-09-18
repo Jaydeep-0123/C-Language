1e9b62334ef56519223690891559aac25e4e7f04
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-18 20:58:42 +0530

    Add files via upload

---
+++ 032_Basicdsa.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct Node
+{ 
+    void insert(int val)
+    {
+       while(Head!=NULL)
+       {
+        p->data->next=next->data->next;
+        printf("p->data %d",data);
+       }
+    }
+}    
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        insert(i*10);
+    }  
+    return 0;  
+}
+
+    
+
